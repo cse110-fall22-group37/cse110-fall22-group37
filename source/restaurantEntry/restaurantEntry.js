17902de492d93e0c3aff331bda9dc14c7ed91873
laurennchen <lmchen@ucsd.edu>
2022-11-14 19:18:39 -0800

    modified css for entries on left side

--- source/restaurantEntry/restaurantEntry.js
+++ source/restaurantEntry/restaurantEntry.js
@@ -17,81 +17,85 @@ class RestaurantEntry extends HTMLElement {
       this.elementStyle = document.createElement('style');
   
       // A4. TODO - Insert all of the styles from cardTemplate.html into the <style> element you just made
-      this.elementStyle.textContent = `* {
-                                      font-family: sans-serif;
-                                      margin: 0;
-                                      padding: 0;
-                                    }
+      this.elementStyle.textContent = 
+      `* {
+        font-family: sans-serif;
+        margin: auto;
+        padding: 0;
+      }
                                   
-                                    a {
-                                      text-decoration: none;
-                                    }
+      a {
+        text-decoration: none;
+      }
                                   
-                                    a:hover {
-                                      text-decoration: underline;
-                                    }
+      a:hover {
+        text-decoration: underline;
+      }
                                   
-                                    article {
-                                      align-items: center;
-                                      border: 1px solid rgb(223, 225, 229);
-                                      border-radius: 8px;
-                                      display: grid;
-                                      grid-template-rows: 118px 56px 14px 18px 15px 36px;
-                                      height: auto;
-                                      row-gap: 5px;
-                                      padding: 0 16px 16px 16px;
-                                      width: 178px;
-                                    }
+      article {
+        align-items: center;
+        border: 1px solid rgb(223, 225, 229);
+        border-radius: 100px;
+        display: grid;
+        row-gap: 10px;
+        grid-template-rows: 118px 56px 14px 18px 15px 36px;
+        height: auto;
+        padding: 16px 16px 16px 16px;
+        width: 600px;
+      }
                                   
-                                    div.rating {
-                                      align-items: center;
-                                      column-gap: 5px;
-                                      display: flex;
-                                    }
+      div.rating {
+        align-items: center;
+        column-gap: 5px;
+        display: flex;
+      }
                                   
-                                    div.rating>img {
-                                      height: auto;
-                                      display: inline-block;
-                                      object-fit: scale-down;
-                                      width: 78px;
-                                    }
+      div.rating>img {
+        height: auto;
+        display: inline-block;
+        object-fit: scale-down;
+        width: 78px;
+      }
+      
+      article>img {
+        border-top-left-radius: 80px;
+        border-top-right-radius: 80px;
+        border-bottom-left-radius: 80px;
+        border-bottom-right-radius: 80px;
+        height: 150px;
+        object-fit: cover;
+        margin-right: 10px;
+        width: 340px;
+        height: 200px
+      }
                                   
-                                    article>img {
-                                      border-top-left-radius: 8px;
-                                      border-top-right-radius: 8px;
-                                      height: 118px;
-                                      object-fit: cover;
-                                      margin-left: -16px;
-                                      width: calc(100% + 32px);
-                                    }
+      p.ingredients {
+        height: 32px;
+        line-height: 16px;
+        padding-top: 4px;
+        overflow: hidden;
+      }
                                   
-                                    p.ingredients {
-                                      height: 32px;
-                                      line-height: 16px;
-                                      padding-top: 4px;
-                                      overflow: hidden;
-                                    }
+      p.organization {
+        color: black !important;
+      }
                                   
-                                    p.organization {
-                                      color: black !important;
-                                    }
+      p.title {
+        display: -webkit-box;
+        font-size: 16px;
+        height: 36px;
+        position: absolute;
+        line-height: 0;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;
+      }
                                   
-                                    p.title {
-                                      display: -webkit-box;
-                                      font-size: 16px;
-                                      height: 36px;
-                                      line-height: 18px;
-                                      overflow: hidden;
-                                      -webkit-line-clamp: 2;
-                                      -webkit-box-orient: vertical;
-                                    }
-                                  
-                                    p:not(.title),
-                                    span,
-                                    time {
-                                      color: #70757A;
-                                      font-size: 12px;
-                                    }`;
+      p:not(.title),
+      span,
+      time {
+        color: black;
+        font-size: 12px;
+      }`;
   
       // A5. TODO - Append the <style> and <article> elements to the Shadow DOM
       this.shadowRC.appendChild(this.elementStyle);
