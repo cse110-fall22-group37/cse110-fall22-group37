1f88d495209cc397cd61ade0e3b5d7aef4311188
jmcole9 <97640992+jmcole9@users.noreply.github.com>
2022-12-02 23:11:43 -0800

    Search Bar works; Deleted search-bar.html

--- source/search-bar/search-bar.js
+++ source/search-bar/search-bar.js
@@ -1,70 +1,32 @@
 /**
- * The init() function is run after the webpage is finished
- * loading
- */
-window.addEventListener('DOMContentLoaded', init);
-
-/**
- * Creates a list of the restaurants from local storage
- */
-function getRestaurantsFromStorage() {
-  return JSON.parse(localStorage.getItem('entries'));
-}
-
-/**
- * When the webpage loads, the restaurants from storage
- * are added to the list
- */
-function init() {
-  let restaurants = getRestaurantsFromStorage();
-  let resList = document.getElementById('resList');
-
-  addRestaurantsToList(resList, restaurants);
-}
-
-/**
- * Takes in an array of restaurants and for each adds it
- * to the existing list
- * @param {Array<Object>} recipes An array of restaurants
- */
-function addRestaurantsToList(resList, restaurants) {
-  for (let i = 0; i < restaurants.length; i++) {
-    let li = document.createElement("li");
-    let a = document.createElement("a");
-    a.href = "#";
-    a.innerText = restaurants[i].name;
-
-    li.appendChild(a);
-    resList.appendChild(li);
-  }
-}
-
-/**
  * When user types in search field, non-matching results 
  * are hidden
  */
 function search() {
-   // Declare variables
+  // Declare variables
   var input, filter, a, i, txtValue;
   input = document.getElementById('myInput');
   filter = input.value.toUpperCase();
 
-  let ul = document.getElementById("resList");
-  let li = ul.getElementsByTagName('li');
-console.log(li)
+  // Selects list of restaurant entries
+  let ul = document.getElementById("list");
+  let li = ul.getElementsByTagName('restaurant-entry');
+
   // Loop through all list items, and hide those who don't match the search query
   for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];   
-    txtValue = a.textContent || a.innerText;
 
+    // Selects the name of the restaurant entry
+    let root =  li[i].shadowRoot;
+    let article = root.querySelector('article');
+    let name = article.querySelector('.name');
+    txtValue = name.innerText;
+
+    // If the text value in the search bar does not match a restaurant entry, it is hidden
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
       li[i].style.display = "";
     } else {
       li[i].style.display = "none";
-      li[i].className = 'hide'
+      li[i].className = 'hide';
     }
-
-    console.log(li[i])
   }
-
 }
